0275032eee37b0c44096b4b58f906b19245503bf
mengxiangtong <928593829@qq.com>
2016-03-24 11:50:58 +0800

    2

---
+++ 8_页面传值_代理/8_页面传值/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  8_页面传值
+//
+//  Created by lanou3g on 14-12-9.
+//  Copyright (c) 2014年 Lanou_zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ 8_页面传值_代理/8_页面传值/MXTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MXTAppDelegate.h
+//  8_页面传值
+//
+//  Created by lanou3g on 14-12-9.
+//  Copyright (c) 2014年 Lanou_zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
